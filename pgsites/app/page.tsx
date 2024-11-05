bbada53d9a52dc23c695258d53f0587a25d951ae
radiallogic <radiallogic@Maxs-Air.fritz.box>
2024-11-05 16:28:48 +0000

    mui added

--- pgsites/app/page.tsx
+++ pgsites/app/page.tsx
@@ -1,13 +1,31 @@
 "use client" 
+
+import React, { useState } from "react";
 import { WindRose } from "./components/WindRose"
-// import {useDetail} from "./hooks/useDetail"
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
 
 export default async function Home() {
-
-  // const records = []
+  const [generateButton, setGenerateButton] = useState(false);
 
   return (
-    <></>
-    // <WindRose sites={ } />
+    <>
+
+<Button variant="outlined">Login</Button><Button variant="outlined">Register</Button>
+
+    <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue="Hello World"
+        />
+        <Button variant="outlined">Search</Button>
+
+
+
+        {/* <List /> */}
+
+    </>
   );
 }
